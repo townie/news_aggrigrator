6fd8b470fcdadcf716b1e840a2a349e9ba955b7a
Keith Webber <kwebber@backupify.com>
2015-02-28 15:18:31 -0500

    converted articles_array into artciles_hash

--- server.rb
+++ server.rb
@@ -8,20 +8,29 @@ end
 
 articles_array = []
 
-get '/articles' do
-  # binding.pry
-  articles_array = CSV.read("./articles.csv")
-  erb :news_home, locals: { articles_array: articles_array }
+def load_articles
+  CSV.read("./articles.csv")
 end
 
-# get '/articles/new' do
-#   redirect '/articles/new/error_pass' #redundant
-# end
+def sites_hash
+  articles_array = load_articles
+  articles_hash = {}
+
+  articles_array.each do |article|
+    articles_hash[article[1]] = { "article" => article[0] ,"description" => article[2]}
+  end
+
+  articles_hash
+end
+
+get '/articles' do
+  articles_hash = sites_hash
+  erb :news_home, locals: { articles_hash: articles_hash }
+end
 
 get '/articles/new' do
   erb :submissions, locals: {error_code: session["error"]}
 end
-
 
 post '/articles/new' do
   if params["title"] == "" || params["URL"] == "" || params["description"] == ""
